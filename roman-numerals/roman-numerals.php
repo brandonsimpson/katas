263edbe41693da6f93a750e7d5493f66009d09ed
Brandon Simpson <brandonsimpson@gmail.com>
2015-05-06 17:43:14 -0500

    arabic to roman tests pass

--- roman-numerals/roman-numerals.php
+++ roman-numerals/roman-numerals.php
@@ -2,10 +2,27 @@
 class Convert
 {
     
+    // roman numeral => numeric equivalents
+    private $roman = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    
     public function ArabicToRoman($input) {
         
-        return null;
-
+        $num = $input;
+        $result = '';
+        
+        foreach ($this->roman as $r => $val) {
+            
+            // divide remaining num by the numeric equivalent val to find how many times to repeat roman numeral chars
+            $matches = intval($num / $val);
+            
+            // build result string of roman numerals
+            $result.= str_repeat($r, $matches);
+            
+            // reduce num by numeric equivalent val
+            $num = $num % $val;
+        }
+        
+        return $result;
     }
     
     public function RomanToArabic($input) {
